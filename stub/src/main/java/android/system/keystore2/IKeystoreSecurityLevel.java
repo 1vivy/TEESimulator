7f148383ef55b28267865edb4f6aa4f635723548
1vivy <1vivy@tutanota.com>
2026-07-29 17:37:52 -0600

    Update keystore stub contracts
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- stub/src/main/java/android/system/keystore2/IKeystoreSecurityLevel.java
+++ stub/src/main/java/android/system/keystore2/IKeystoreSecurityLevel.java
@@ -13,6 +13,10 @@ public interface IKeystoreSecurityLevel extends IInterface {
                             KeyParameter[] params, int flags, byte[] entropy);
 
     class Stub {
+        static final int TRANSACTION_createOperation = IBinder.FIRST_CALL_TRANSACTION;
+        static final int TRANSACTION_generateKey = IBinder.FIRST_CALL_TRANSACTION + 1;
+        static final int TRANSACTION_importKey = IBinder.FIRST_CALL_TRANSACTION + 2;
+
         public static IKeystoreSecurityLevel asInterface(IBinder b) {
             throw new UnsupportedOperationException("STUB!");
         }
--- stub/src/main/java/android/system/keystore2/IKeystoreService.java
+++ stub/src/main/java/android/system/keystore2/IKeystoreService.java
@@ -8,6 +8,13 @@ public interface IKeystoreService {
     IKeystoreSecurityLevel getSecurityLevel(int securityLevel);
 
     class Stub {
+        static final int TRANSACTION_getSecurityLevel = IBinder.FIRST_CALL_TRANSACTION;
+        static final int TRANSACTION_getKeyEntry = IBinder.FIRST_CALL_TRANSACTION + 1;
+        static final int TRANSACTION_updateSubcomponent = IBinder.FIRST_CALL_TRANSACTION + 2;
+        static final int TRANSACTION_listEntries = IBinder.FIRST_CALL_TRANSACTION + 3;
+        static final int TRANSACTION_deleteKey = IBinder.FIRST_CALL_TRANSACTION + 4;
+        static final int TRANSACTION_listEntriesBatched = IBinder.FIRST_CALL_TRANSACTION + 8;
+
         public static IKeystoreService asInterface(IBinder b) {
             throw new UnsupportedOperationException("STUB!");
         }
